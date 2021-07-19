7ada98399a47cc812c19ae097f813ed4fcc998f0
atilavirginia1 <atilavirginia1@gmail.com>
2021-07-19 14:26:12 -0300

    Phone mask w/ jquery

--- marcacao.js
+++ marcacao.js
@@ -12,5 +12,11 @@ $("#datepicker").on('click', function () {
   return false;
 });
 
-
-
+$('#fone').mask('(00) 0000-00009');
+$('#fone').on('keyup', function(event) {
+   if($(this).val().length == 15){ // Celular com 9 dígitos + 2 dígitos DDD e 4 da máscara
+      $('#fone').mask('(00) 00000-0009');
+   } else {
+      $('#fone').mask('(00) 0000-00009');
+   }
+});
